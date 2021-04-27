766936e57634bb0c20225a7c37bef1ef75296251
Aleksa Petrovic <aleksa.petrovic2907@gmail.com>
2021-04-27 21:41:43 +0200

    deleted invalid key

--- db/migrate/20210427125358_add_nick_to_users.rb
+++ db/migrate/20210427125358_add_nick_to_users.rb
@@ -1,6 +1,6 @@
 class AddNickToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :nick, :string
-    add_index :users, :nick, unique: true, presence: true
+    add_index :users, :nick, unique: true
   end
 end
